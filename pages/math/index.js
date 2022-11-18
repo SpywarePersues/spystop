0466ab43fe68bfa14d24f31f905d509ce68fafb0
SpywarePersues <spywarepersues284@gmail.com>
2022-11-18 15:55:17 +0530

    More coming soon card added

--- pages/math/index.js
+++ pages/math/index.js
@@ -181,6 +181,16 @@ function index() {
                     </div>
                     </Link>
                 </div>
+                <div className='dark-glassmorph p-3'>
+                    <h1 className='text-3xl text-white font-Bebas text-center my-10'>More Coming Soon.</h1>
+                    <div className="text-center">
+                        <button class="relative font-Bebas text-2xl inline-flex items-center justify-center p-0.5 mr-2 overflow-hidden font-medium text-gray-900 rounded-lg group bg-gradient-to-br from-pink-500 to-blue-500 group-hover:from-pink-500 group-hover:to-blue-500 hover:text-white dark:text-white focus:ring-4 focus:outline-none">
+                        <span class="relative px-5 py-2 transition-all ease-in duration-75 bg-white dark:bg-gray-900 rounded-md group-hover:bg-opacity-0">
+                            Coming Soon.
+                        </span>
+                        </button>
+                    </div>
+                </div>
             </div>
             <Footer />
         </div>
