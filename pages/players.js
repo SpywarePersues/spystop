08dccde0f9718c759aa62d44de9e68f4b103becb
SpywarePersues <spywarepersues284@gmail.com>
2022-11-19 12:37:52 +0530

    Title bug fixed

--- pages/players.js
+++ pages/players.js
@@ -54,7 +54,7 @@ export default function players(){
 
     return(
         <div className=''>
-            <Navbar title="Store" />
+            <Navbar title="Players" />
             <img src="./wave-bg.png" alt="wave" className="fixed top-0 -z-10 h-screen"/>
             <h1 className='text-6xl font-Bungee text-center pb-5 pt-5 font-semibold'>Players</h1>
             {token ? (<div className='my-10 p-10 grid grid-cols-1 sm:grid-cols-1 md:grid-cols-1 lg:grid-cols-1 xl:grid-cols-1'>
